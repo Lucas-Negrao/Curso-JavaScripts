fd8cc8493c313835d43174296e6daa2e08de8739
Lucas-Negrao <negraolucas98@gmail.com>
2024-08-15 14:49:14 -0300

    organizando modulos
    exercícios de multas

---
+++ Modulo D/aula011/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 60.8
+console.log(`A velocidade do carro é de ${velocidade}`)
+//condição simples
+if (velocidade > 60){
+    console.log('Você passou o limite de velocidade. MULTADO!')         
+}
+console.log('Dirija com cuidado')
---
+++ Modulo D/aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+
+if (pais!='BR'){
+    console.log('Bem vindo ao Brasil')
+}
+
+else{
+    console.log('O bom filho a casa torna')
+}
