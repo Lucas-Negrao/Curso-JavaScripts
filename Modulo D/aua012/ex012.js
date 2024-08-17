4ee8066ce550ad96e2efb532cfb63313bb3a0a4c
Lucas-Negrao <negraolucas98@gmail.com>
2024-08-17 14:48:56 -0300

    creat ex012
    treino no JS para um sistema de votos

---
+++ Modulo D/aua012/ex012.js
@@ -0,0 +1,12 @@
+var idade = 65
+console.log(`Você tem ${idade} anos`)
+if (idade<16) {
+    console.log('Você ainda não pode votar')
+}
+else if (idade >=16 && idade <18 || idade>65){
+        console.log('Seu voto é opcional')
+}
+else if (idade>=18){
+    console.log('Seu voto é obrigatorio!')
+}
+
