6e3653817e6b6892e2d0f3fe791649d8c560f3ee
Lucas-Negrao <negraolucas98@gmail.com>
2024-09-17 13:57:42 -0300

    exercicios de fatorial feitos

---
+++ modulo F/aula 016/par-impar.js
@@ -0,0 +1,13 @@
+function acao(n){
+    if (n%2==0){
+        return 'par'
+    }
+    else{
+        return 'impar'
+    }
+}
+
+
+let numero = acao(4)
+
+console.log(numero)
---
+++ modulo F/aula 016/soma.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0){
+    return n1+n2
+}
+
+let numero = soma(92)
+
+console.log(numero)
