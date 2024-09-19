a2765c626eaf6ccb0e52627eb0c093959067162f
Lucas-Negrao <negraolucas98@gmail.com>
2024-09-19 11:46:02 -0300

    finalizando
    terminei o curso JS

--- modulo F/solução-professor/arrays/arrays.js
+++ modulo F/solução-professor/arrays/arrays.js
@@ -10,21 +10,39 @@ function isnumero(n){
 
 
 function inlista(n, l){
-    if (l.indexOf(Number(n)) !=-1){
-        return true
-    }
-    else{
-        return false
-    }
+    return l.indexOf(Number(n)) !=-1
 }
+    
+  
 
 function adicionar(){
     let num = Number(numero.value)
     if (isnumero(num) && !inlista(num, valores)){
-        window.alert('ok')
+        valores.push(Number(num))
+        let item = document.createElement('option')
+        item.textContent = `Adicionou o numero ${num}`
+        seletor.appendChild(item)
+        
     }
     else{
         window.alert('invalido')
     }
-    
+}
+
+function finalizar(){
+    if (valores==''){
+        window.alert('Lista vazia')
+    }
+    else{
+        let total= valores.length
+        let soma = valores.reduce((acumulador, valoratual) => acumulador + valoratual)
+        let media = soma/total
+
+        resposta.innerHTML =''
+        resposta.innerHTML+=`Sua lista tem ${valores.length} números <br>`
+        resposta.innerHTML+= `O maior número é ${Math.max(...valores)} <br>` 
+        resposta.innerHTML+= `O menor número é ${Math.min(...valores)} <br>`
+        resposta.innerHTML+= `Somando todos os números o resultado é ${soma} <br>`
+        resposta.innerHTML+= ` media é ${media}`
+    }
 }
